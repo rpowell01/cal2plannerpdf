9674b04b8e1a0687a7d47bc465da4a166d50aeb8
russell powell <russell.powell@tierpoint.com>
2025-11-28 12:10:14 -0600

    add logging, task schedule ps1 script and update readme.md appropriately

--- cal2plannerpdf-taskscheduler.ps1
+++ cal2plannerpdf-taskscheduler.ps1
@@ -1,3 +1,10 @@
+#
+# Note: when using windows task scheduler to execute the python script via a scheduled
+# task, you must select the option "Run only when user is logged in".  This requirement
+# is due to the fact that the python script needs to call / dispatch the outlook client
+# in order to retrieve its calendar events. The outlook client requires a desktop
+# environment and you only get a desktop environment for a logged in user.
+#
 Set-Location -Path $PSScriptRoot
 .\.venv\Scripts\Activate.ps1
-python .\cal2plannerpdf.py --autostart
+.\.venv\Scripts\python.exe .\cal2plannerpdf.py --autostart
